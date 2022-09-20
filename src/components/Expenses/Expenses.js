d2845cc4f7f729bbad32894f80fb4cff66452f53
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2022-09-20 15:52:00 +0200

    add expense filter state

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,22 +1,37 @@
+import { useState } from "react";
+
 import Card from "../UI/Card";
 import ExpenseItem from ".//ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 
 import "./Expenses.css";
 
 const Expenses = ({ expenses }) => {
+	const [filteredYear, setFilteredYear] = useState("2022");
+
+	const filteredChangeHadler = (selectedYear) => {
+		setFilteredYear(selectedYear);
+	};
+
 	return (
-		<Card className="expenses">
-			{expenses.map((expense) => {
-				return (
-					<ExpenseItem
-						key={expense.id}
-						title={expense.title}
-						amount={expense.amount}
-						date={expense.date}
-					/>
-				);
-			})}
-		</Card>
+		<div>
+			<Card className="expenses">
+				<ExpensesFilter
+					selected={filteredYear}
+					onChageFilter={filteredChangeHadler}
+				/>
+				{expenses.map((expense) => {
+					return (
+						<ExpenseItem
+							key={expense.id}
+							title={expense.title}
+							amount={expense.amount}
+							date={expense.date}
+						/>
+					);
+				})}
+			</Card>
+		</div>
 	);
 };
 
